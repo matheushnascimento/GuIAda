60a5a66a4ca082e07e9aa1594bacd72d4d9c6fec
Matheus Nascimento <matheus.mhddn@gmail.com>
2023-12-08 10:14:37 -0300

    footer css style and year update with js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 const button = document.getElementById("burger_button");
+const currentYear = document.getElementById("year");
 
 button.onclick = () => {
   const nav = document.querySelector("nav");
   nav.classList.toggle("hide");
 };
+
+currentYear.innerHTML = `&copy; ${new Date().getFullYear()} Emter Sistema 
+Todos os direitos reservados`;
